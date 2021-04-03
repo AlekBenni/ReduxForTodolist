b3488a8276f10fb5d3acae1e3b88137fe82dd010
Alek Benny <adeyva@mail.ru>
2021-04-03 19:59:14 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { removeTaskAC, addTaskAC, changeStatusAC, onChangeTitleAC } from './stat
 import { useDispatch, useSelector } from 'react-redux';
 import { RootStateType } from './state/store';
 
-const test = 2
+const test = 3
 
 const useStyles = makeStyles((theme) => ({
     root: {
